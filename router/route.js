b50385a5f02fcf149f593470e87afacd9b638fbd
Za_Genius <rapzter58@gmail.com>
2023-12-13 00:40:51 +0100

    feat: joi validation

--- router/route.js
+++ router/route.js
@@ -1,15 +1,46 @@
 
 const express = require('express');
-// import {validate} from '../middleware/validate.js';
+const { checkSchema, body, validationResult } = require('express-validator');
 const  userController  = require('../controller/userController.js');
 const {findUsers, findUserById, createUser, updateUser, deleteUser} = userController;
 const router = express.Router();
-
+const schema = require('../validation/schema/joiUserSchema.js');
 
 router.get('/users',findUsers)
 router.get('/users/:id',findUserById)
 
-router.post('/users', createUser)
+router.post('/users',
+    (req,res,next)=>{
+        const user = req.body;
+         
+        const {error: vError} = schema.validate({...user});
+
+        if(vError){
+            // return res.status(400).json({
+            //     message: 'bad user input',
+            //     statusCode: 400,
+            //     error: errorbody,
+            // })
+            return res.status(400).send(vError)
+        }
+        return next();
+        // try {
+        // }
+        // catch (err) { 
+
+
+            
+        //     const errorbody = {}
+        //     console.log('validation error ',err);
+        //     err.details.forEach((e)=>{
+        //         errorbody[e.context.key] = e.message;
+        //     })
+
+           
+        // }
+    },
+ createUser
+ );
 router.put('/users/:id', updateUser)
 
 router.delete('/users/:id', deleteUser)
